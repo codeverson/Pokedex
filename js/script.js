9312318817e6101d88f110f8d64fa47d2b13e859
Everson Araujo <araujoeverson@icloud.com>
2023-05-01 17:22:54 -0300

    JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon (pokemon);
+
+    
+
+}
+
+renderPokemon('25')
