6fae462ee0730c1c3ddce330b5c8e995bfa43167
Everson Araujo <araujoeverson@icloud.com>
2023-05-01 17:46:44 -0300

    Pesquisa pronta

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number'); 
 const pokemonImage = document.querySelector('.pokemon__image'); 
 
+const form = document.querySelector('.form'); 
+const input = document.querySelector('.input__search'); 
+
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -17,3 +20,12 @@ const renderPokemon = async (pokemon) => {
     pokemonNumber.innerHTML = data.id
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 }
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+
+    input.value = "";
+
+});
