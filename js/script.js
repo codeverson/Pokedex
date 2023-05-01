fe4c58e32e6ce5693c7824394271e47633062df0
Everson Araujo <araujoeverson@icloud.com>
2023-05-01 17:37:09 -0300

    final da API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number'); 
+const pokemonImage = document.querySelector('.pokemon__image'); 
+
+
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIResponse.json();
@@ -8,8 +13,7 @@ const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon (pokemon);
 
-    
-
+    pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 }
-
-renderPokemon('25')
